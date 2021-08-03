f47c73e1855eb84ae6ec70170f5630a856d84a70
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-08-03 17:59:00 +0600

    some demo data added to all fields

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=200, null=True)
 
+    def __str__(self):
+        return self.name
+
 class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=50, null=True)

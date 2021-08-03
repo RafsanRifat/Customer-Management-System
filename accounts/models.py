c9ba4fb7a1ad3519f142e5955a0cd8b344a9fde9
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-08-03 13:27:47 +0600

    two more fields created in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,34 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.TextField(max_length=1000, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Orders(models.Model):
+    STATUS = (
+        ('pending', 'pending'),
+        ('Out For Delivery', 'Out For Delivery'),
+        ('Delivered', 'Delivered')
+    )
+    #customer
+    #order
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self):
+        return self.status
+

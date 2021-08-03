7ddb6d4bcfc027c5d4b98d5e50711100a54acc2d
Md.Rafsanjani <rafsan.rifat255@gmail.com>
2021-08-03 11:50:26 +0600

    migrated

--- Customer_management_system/settings.py
+++ Customer_management_system/settings.py
@@ -124,6 +124,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
